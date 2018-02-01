2db70933d3e3256d877afa9f79a63987d78e53c3
Aubrey Keus <aubrey@brave.com>
2018-01-31 22:15:50 -0500

    Remove Linux symbols (as they are not yet working correctly)
    Auditors: @bbondy, @bridiver

--- fetch.sh
+++ fetch.sh
@@ -15,13 +15,11 @@ do
 		( cd symbols/win32-ia32-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-win32-ia32-symbols.zip | $cmd )
 		( cd symbols/win32-x64-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-win32-x64-symbols.zip | $cmd )
 		( cd symbols/darwin-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-darwin-x64-symbols.zip | $cmd )
-		( cd symbols/linux-$VERSION; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-linux-x64-symbols.zip | $cmd )
 	elif [ `which unzip` ]; then
 		cmd="unzip -x"
 		( cd symbols/win32-ia32-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-win32-ia32-symbols.zip > /tmp/win32-ia32.zip ; $cmd /tmp/win32-ia32.zip )
 		( cd symbols/win32-x64-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-win32-x64-symbols.zip > /tmp/win32-x64.zip ; $cmd /tmp/win32-x64.zip )
 		( cd symbols/darwin-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-darwin-x64-symbols.zip > /tmp/darwin.zip ; $cmd /tmp/darwin.zip )
-		( cd symbols/linux-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-linux-x64-symbols.zip > /tmp/linux.zip ; $cmd /tmp/linux.zip )
 	fi
 
 done
