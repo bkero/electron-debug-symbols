1bcd5e4368383de8e4c10d0c21d149050f88e715
Ben Kero <bkero@bke.ro>
2018-07-09 13:07:12 -0700

    Cleanup fetch.sh script, pass shellcheck

--- fetch.sh
+++ fetch.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
+set -eux
 
 if [ -z "$BRAVE_VERSIONS" ]; then
     echo "Need to set BRAVE_VERSIONS to at least one brave/electron version"
     exit 1
 fi
 
-for VERSION in `echo $BRAVE_VERSIONS`
+for VERSION in $BRAVE_VERSIONS
 do
+    # shellcheck disable=SC2086
+    mkdir -p symbols/{win32-ia32-$VERSION,win32-x64-$VERSION,darwin-$VERSION,linux-$VERSION}
 
-	mkdir -p symbols/{win32-ia32-$VERSION,win32-x64-$VERSION,darwin-$VERSION,linux-$VERSION}
-
-	if [ `which bsdtar` ] && [ ! $FORCE_UNZIP ]; then
-		cmd="bsdtar -xvf-"
-		( cd symbols/win32-ia32-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-win32-ia32-symbols.zip | $cmd )
-		( cd symbols/win32-x64-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-win32-x64-symbols.zip | $cmd )
-		( cd symbols/darwin-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-darwin-x64-symbols.zip | $cmd )
-	elif [ `which unzip` ]; then
-		cmd="unzip -x"
-		( cd symbols/win32-ia32-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-win32-ia32-symbols.zip > /tmp/win32-ia32.zip ; $cmd /tmp/win32-ia32.zip )
-		( cd symbols/win32-x64-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-win32-x64-symbols.zip > /tmp/win32-x64.zip ; $cmd /tmp/win32-x64.zip )
-		( cd symbols/darwin-$VERSION ; wget -O - https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-darwin-x64-symbols.zip > /tmp/darwin.zip ; $cmd /tmp/darwin.zip )
-	fi
+    if [ "$(which bsdtar)" ] && [ ! "$FORCE_UNZIP" ]; then
+        cmd="bsdtar -xvf-"
+        ( cd "symbols/win32-ia32-$VERSION" ; wget -O - "https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-win32-ia32-symbols.zip" | $cmd )
+        ( cd "symbols/win32-x64-$VERSION" ; wget -O - "https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-win32-x64-symbols.zip" | $cmd )
+        ( cd "symbols/darwin-$VERSION" ; wget -O - "https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-darwin-x64-symbols.zip" | $cmd )
+    elif [ "$(which unzip)" ]; then
+        cmd="unzip -x"
+        ( cd "symbols/win32-ia32-$VERSION" ; wget -O - "https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-win32-ia32-symbols.zip" > /tmp/win32-ia32.zip ; $cmd /tmp/win32-ia32.zip ; rm /tmp/win32-ia32.zip)
+        ( cd "symbols/win32-x64-$VERSION" ; wget -O - "https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-win32-x64-symbols.zip" > /tmp/win32-x64.zip ; $cmd /tmp/win32-x64.zip ; rm /tmp/win32-x64.zip)
+        ( cd "symbols/darwin-$VERSION" ; wget -O - "https://github.com/brave/electron/releases/download/v${VERSION}/brave-v${VERSION}-darwin-x64-symbols.zip" > /tmp/darwin.zip ; $cmd /tmp/darwin.zip ; rm /tmp/darwin.zip)
+    fi
 
 done
